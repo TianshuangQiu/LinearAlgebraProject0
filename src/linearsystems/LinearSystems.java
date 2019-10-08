a33dd03f60814031681658a19addb65d0d08ba1a
Ethan Qiu SCH <balancein0and1@gmail.com>
2019-10-08 18:03:56 -0400

    Create LinearSystems.java

---
+++ src/linearsystems/LinearSystems.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linearsystems;
+
+import java.util.*;
+/**
+ *
+ * @author eqiu
+ */
+public class LinearSystems {
+
+    static ArrayList <ArrayList<Integer>> storageMatrix = new ArrayList<>();
+    static int[][] solveMatrix;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+
+}
